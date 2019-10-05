a95236f06f50e9017ee5ec4335497670c0cf1397
magdolenp <patrik7496@gmail.com>
2019-10-05 17:44:50 +0200

    chore(app): package update

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuSection {
+  name: string;
+  section: string;
+}
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
+  readonly menuSections: MenuSection[] = [
+    { name: 'Calculators', section: 'calc' },
+    { name: 'About', section: 'about' },
+  ];
+
   constructor() {}
 }
