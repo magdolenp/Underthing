698999e3e56a6e9db596349bb2324e8bb05078a1
Michal <michaell.novota@gmail.com>
2021-03-29 22:32:48 +0200

    feat(monster-detail): stats typescript, monster logo

--- src/app/content/monsters/monster-detail/monster-detail.component.ts
+++ src/app/content/monsters/monster-detail/monster-detail.component.ts
@@ -6,4 +6,17 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./monster-detail.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MonsterDetailComponent {}
+export class MonsterDetailComponent {
+  readonly monsterStat: { key: string; value: number }[] = [
+    { key: 'strength', value: 24 },
+    { key: 'dexterity', value: 19 },
+    { key: 'constitution', value: 22 },
+    { key: 'intelligence', value: 16 },
+    { key: 'wisdom', value: 15 },
+    { key: 'charisma', value: 18 },
+  ];
+  statModifier(stat: number): string {
+    const modifier = Math.floor((stat - 10) / 2);
+    return `${modifier < 0 ? '' : '+'}${modifier}`;
+  }
+}
